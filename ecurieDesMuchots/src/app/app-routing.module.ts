6a53f2b09500093d075fe5243dbe1ade0fb5d052
Ivrro <ivrromf@gmail.com>
2021-06-19 01:48:08 +0200

    fix scss

--- ecurieDesMuchots/src/app/app-routing.module.ts
+++ ecurieDesMuchots/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
 import { InstallationsListComponent } from './installations/installations-list/installations-list.component';
+import { PrestationsComponent } from './prestations/prestations.component';
 
 
 const routes: Routes = [
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'installations',
     component: InstallationsListComponent
+  },
+  {
+    path: 'prestations',
+    component: PrestationsComponent
   }
 ];
 
--- ecurieDesMuchots/src/app/app.component.ts
+++ ecurieDesMuchots/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class AppComponent {
 
