61032339901e6d492cf6b2bc87ccf3d1da131972
Ninon Jordao <ninon.jordao@outlook.fr>
2021-06-19 01:31:34 +0200

    fix bug

--- ecurieDesMuchots/src/app/installations/installation.service.ts
+++ ecurieDesMuchots/src/app/installations/installation.service.ts
@@ -1,7 +1,9 @@
+import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
 import { Installation } from "./installation";
 import { INSTALLATIONS } from "./mock-installations";
 
+@Injectable({ providedIn: 'root' })
 export class InstallationService {
     constructor(){}
 
