66d366f5b8ad38cefe5766cc8ae4c8aa74ebe5be
Ninon Jordao <ninon.jordao@outlook.fr>
2021-06-19 01:50:36 +0200

    installations-detail

--- ecurieDesMuchots/src/app/installations/installations-detail/installations-detail.component.ts
+++ ecurieDesMuchots/src/app/installations/installations-detail/installations-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-installations-detail',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./installations-detail.component.css']
 })
 export class InstallationsDetailComponent implements OnInit {
+
+  @Input() image;
+  @Input() title; 
+  //@Input() description; 
 
   constructor() { }
 
