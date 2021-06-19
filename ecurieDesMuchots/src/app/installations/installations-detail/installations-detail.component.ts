c217dcbc8a57e32bbef5066cb847d17958d22a7d
Ninon Jordao <ninon.jordao@outlook.fr>
2021-06-19 02:18:55 +0200

    css details

--- ecurieDesMuchots/src/app/installations/installations-detail/installations-detail.component.ts
+++ ecurieDesMuchots/src/app/installations/installations-detail/installations-detail.component.ts
@@ -3,13 +3,14 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-installations-detail',
   templateUrl: './installations-detail.component.html',
-  styleUrls: ['./installations-detail.component.css']
+  styleUrls: ['./installations-detail.component.scss']
 })
 export class InstallationsDetailComponent implements OnInit {
 
   @Input() image;
   @Input() title; 
   @Input() description; 
+  @Input() leftRight;
 
   constructor() { }
 
--- ecurieDesMuchots/src/app/installations/mock-installations.ts
+++ ecurieDesMuchots/src/app/installations/mock-installations.ts
@@ -2,5 +2,6 @@ import { Installation } from "./installation";
 
 
 export const INSTALLATIONS: Installation[] = [
-  { img : "assets/img/carriere1.jpeg", title: 'Carrière', description: 'Une grande carrière extérieure avec du sable fin' }
+  { img : "assets/img/carriere1.jpeg", title: 'Carrière', description: 'Une grande carrière extérieure avec du sable fin' },
+  { img : "assets/img/box1.jpg", title: "Boxs", description: "Des boxs de 3*3 "}
 ];
